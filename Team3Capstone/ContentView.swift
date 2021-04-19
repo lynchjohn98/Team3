96822edca17d6a49d5385e11c4849ad874e8d7d4
John Lynch <lynchjohn98@gmail.com>
2021-04-19 16:51:51 -0500

    Initial Commit

---
+++ Team3Capstone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Team3Capstone
+//
+//  Created by John William Lynch on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team3Capstone/Team3CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team3CapstoneApp.swift
+//  Team3Capstone
+//
+//  Created by John William Lynch on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Team3CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
