15d27c4c5708b1c15a34278aba12129f06895779
John Lynch <lynchjohn98@gmail.com>
2021-04-19 18:28:46 -0500

    Work done after first meeting

--- Team3Capstone/ContentView.swift
+++ Team3Capstone/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-            Text("This is for our capstone THIS iS A TEST")
-        }
     
+    var body: some View {
+        
+        VStack {
+           Spacer()
+            
+            //Title Vertical Stack
+            VStack {
+            Text("Where are the Squirrels?")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+                .background(Image("titlePageSquirrel")
+                                .resizable()
+                                .frame(width:400, height: 400))
+            }
+            
+            Spacer()
+            
+            //Caption vertical stack
+            VStack {
+                Text("Help Monitor the Environmental Conditions that affect our Neighborhood Wildlife")
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            
+            //Get started button vertical stack
+            VStack {
+                Text("Get Started!")
+                    .background(Image("squirrelCartoon")
+                                    .resizable()
+                                    .frame(width:100,height:100))
+                Spacer()
+                    .frame(width: 50.0, height: 100.0)
+            }
+            
+            NavigationView {
+                HStack {
+                    Text("Test")
+                
+                    .toolbar {
+                        
+                        ToolbarItemGroup(placement: .navigationBarLeading) {
+                            
+                            Button("Item 1") {
+                                print("Item 1 clicked")
+                            }
+
+                            Button("Item 2") {
+                                print("Item 2 clicked!")
+                            }
+                            Button("Item 3") {
+                                print("Item 3 clicked")
+                            }
+                            Button("Item 4") {
+                                print("Item 4 clicked")
+                            }
+                        }
+                    }
+                }
+            }
+            .frame(width:200, height:200)
+            
+            
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
